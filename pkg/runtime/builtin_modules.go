c72e8b870f4d7253fe0a24c33a887a75c3c9b33d
Tim Voronov <ziflex@users.noreply.github.com>
2026-06-24 17:40:33 -0400

    Upgrade dependencies to latest release candidates, add net/rest module, and refactor module initialization structure (#50)

--- pkg/runtime/builtin_modules.go
+++ pkg/runtime/builtin_modules.go
@@ -5,37 +5,50 @@ import (
 
 	"github.com/MontFerret/contrib/modules/csv"
 	"github.com/MontFerret/contrib/modules/db/sqlite"
+	"github.com/MontFerret/contrib/modules/net/rest"
 	"github.com/MontFerret/contrib/modules/security/jwt"
 	"github.com/MontFerret/contrib/modules/toml"
 	"github.com/MontFerret/contrib/modules/web/article"
+	"github.com/MontFerret/contrib/modules/web/html"
+	"github.com/MontFerret/contrib/modules/web/html/drivers/cdp"
+	"github.com/MontFerret/contrib/modules/web/html/drivers/memory"
 	"github.com/MontFerret/contrib/modules/web/robots"
 	"github.com/MontFerret/contrib/modules/web/sitemap"
 	"github.com/MontFerret/contrib/modules/xml"
 	"github.com/MontFerret/contrib/modules/yaml"
 	"github.com/MontFerret/ferret/v2/pkg/module"
-
-	"github.com/MontFerret/contrib/modules/web/html"
-	"github.com/MontFerret/contrib/modules/web/html/drivers/cdp"
-	"github.com/MontFerret/contrib/modules/web/html/drivers/memory"
 )
 
+type namespaceInitializer func(opts Options) ([]module.Module, error)
+
 func newModules(opts Options) ([]module.Module, error) {
-	webmods, err := newWebModules(opts)
-
-	if err != nil {
-		return nil, err
-	}
-
-	datamods, err := newDataModules()
-
-	if err != nil {
-		return nil, err
-	}
-
-	return append(webmods, datamods...), nil
+	return initModules(
+		opts,
+		webMods,
+		dataMods,
+		dbMods,
+		securityMods,
+		networkMods,
+	)
 }
 
-func newWebModules(opts Options) ([]module.Module, error) {
+func initModules(opts Options, initializers ...namespaceInitializer) ([]module.Module, error) {
+	var merged []module.Module
+
+	for _, r := range initializers {
+		modules, err := r(opts)
+
+		if err != nil {
+			return nil, err
+		}
+
+		merged = append(merged, modules...)
+	}
+
+	return merged, nil
+}
+
+func webMods(opts Options) ([]module.Module, error) {
 	htmlmod, err := html.New(
 		html.WithDefaultDriver(memory.New(opts.ToInMemory()...)),
 		html.WithDrivers(
@@ -52,16 +65,32 @@ func newWebModules(opts Options) ([]module.Module, error) {
 		sitemap.New(),
 		article.New(),
 		robots.New(),
-		jwt.New(),
-		sqlite.New(),
 	}, nil
 }
 
-func newDataModules() ([]module.Module, error) {
+func dataMods(_ Options) ([]module.Module, error) {
 	return []module.Module{
 		csv.New(),
 		toml.New(),
 		xml.New(),
 		yaml.New(),
+	}, nil
+}
+
+func dbMods(_ Options) ([]module.Module, error) {
+	return []module.Module{
+		sqlite.New(),
+	}, nil
+}
+
+func securityMods(_ Options) ([]module.Module, error) {
+	return []module.Module{
+		jwt.New(),
+	}, nil
+}
+
+func networkMods(_ Options) ([]module.Module, error) {
+	return []module.Module{
+		rest.New(),
 	}, nil
 }
